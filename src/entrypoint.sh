4efcc4e4665c6422679b0084a7a687ed60d772c2
Jay Rogers <jay@521dimensions.com>
2025-01-09 09:13:23 -0600

    Added edge tag and release process

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -12,11 +12,12 @@ if [ ! -f "$FTP_TLS_CERTIFICATE_FILE" ] || [ ! -f "$FTP_TLS_CERTIFICATE_KEY_FILE
                 exit 1
             fi
         done
+    else
+        echo "🔐 SSL Keypair not found. Generating self-signed SSL keypair..."    
+        openssl req -x509 -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost" -nodes -newkey rsa:2048 -keyout "$FTP_TLS_CERTIFICATE_KEY_FILE" -out "$FTP_TLS_CERTIFICATE_FILE" -days 365 >/dev/null 2>&1
+        chmod 600 "${FTP_TLS_CERTIFICATE_KEY_FILE}"
+        echo "✅ SSL Keypair generated..."
     fi
-    echo "🔐 SSL Keypair not found. Generating self-signed SSL keypair..."    
-    openssl req -x509 -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost" -nodes -newkey rsa:2048 -keyout "$FTP_TLS_CERTIFICATE_KEY_FILE" -out "$FTP_TLS_CERTIFICATE_FILE" -days 365 >/dev/null 2>&1
-    chmod 600 "${FTP_TLS_CERTIFICATE_KEY_FILE}"
-    echo "✅ SSL Keypair generated..."
 else
   echo "ℹ️ NOTICE: SSL certificate and private key already exist, so we'll be using the following files:"
   echo "  - ${FTP_TLS_CERTIFICATE_FILE}"
